5563196000911d86e5699e72920b9cd6236b955a
Dumitru Bragari <dumitru@bragari.es>
2024-02-08 22:32:01 +0100

    form

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  public title: string;
+  public subtitle: string;
+  public web: string;
+
+  constructor(){
+    this.title = "Victor Robles";
+    this.subtitle = "Desarollador Web y Formador";
+    this.web = "victorroblesweb.es";
+  }
+  ngOnInit(): void {
+      
+  }
 
 }
--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project';
+import { ProjectService } from '../../services/project.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrl: './create.component.css'
+  styleUrl: './create.component.css',
+  providers: [ProjectService]
 })
-export class CreateComponent {
+export class CreateComponent implements OnInit{
 
+  public title: string;
+  public project: Project;
+
+  constructor(
+    private _projectService: ProjectService
+  ){
+    this.title = "Crear Proyecto"
+    this.project = new Project('','','','',2019,'','');
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: any){
+    console.log(this.project);
+  }
 }
